af646a4988faa30befa00b3ce6212a0688bf427e
vishal14marathe <vishalmarathe@gmail.com>
2026-03-15 14:09:59 +0530

    s

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
       ? process.env.FRONTEND_URL
-      : "https://task-frontend-4zvb.onrender.com",
+      : "http://localhost:5173",
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
